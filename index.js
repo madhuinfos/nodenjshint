ba2bfb0e229bf948b06b9d13938a568e4fed5c39
Madhu <madhuinfos@users.noreply.github.com>
2021-09-25 22:36:36 +0530

    js async programming

--- index.js
+++ index.js
@@ -2,22 +2,40 @@ const express = require('express');
 const logger = require('./logger');
 const app = express();
 
-const courses = [{id: 1, name: 'course1'}, {id:2, name:'course2'}];
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+];
 
-app.get('/api/courses', (req, res) => {
-    logger.log(`Get all courses ${req.url}`);
-    res.status(200).send(courses);
+app.get('/api/courses', async (req, res) => {
+  logger.log(`Get all courses ${req.url}`);
+  getDataWithCallback((data) => console.log(data));
+  getDataWithPromise().then((result) => console.log(result));
+
+  const result = await getDataWithPromise();
+  console.log(result);
+  res.status(200).send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    
-    const course = courses.find(x => x.id === parseInt(req.params.id));
-    if(course){
-        return res.status(200).send(course);
-    }
+  const course = courses.find((x) => x.id === parseInt(req.params.id));
+  if (course) {
+    return res.status(200).send(course);
+  }
 
-    res.sendStatus(404);
+  res.sendStatus(404);
 });
-const portNo = 2000;
+
+function getDataWithCallback(callback) {
+  callback({ name: 'user', program: 'hello world' });
+}
+
+function getDataWithPromise() {
+  return new Promise((resolve, reject) => {
+    resolve({ name: 'user', program: 'hello world' });
+  });
+}
+
+const portNo = process.env.PORT || 2000;
 
 app.listen(portNo, () => console.log(`listening on port ${portNo}`));
